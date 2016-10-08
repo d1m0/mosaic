28ea8498d191ed270cdd82b6f0ac981bacf9ff56
Dimo <lami4ka@gmail.com>
2016-10-08 14:29:09 -0700

    Basic upload form

--- mosaic/forms.py
+++ mosaic/forms.py
@@ -1,3 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, TextAreaField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, BooleanField, TextAreaField, FileField
+from wtforms.validators import DataRequired, Length, regexp
+
+class UploadForm(FlaskForm):
+    video = FileField(u'Video File', validators=[regexp(u'^[^\/\\\\]*\.(mov|mp4|avi|mpg|mpeg)$'), DataRequired()])
+    tags = StringField('tags', default="")
+    location = StringField('location', default="")
+    release = BooleanField('release', default=False)
--- mosaic/views.py
+++ mosaic/views.py
@@ -2,6 +2,7 @@ from mosaic import app, db
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask_login import login_user, logout_user, current_user, login_required
 from datetime import datetime
+from .forms import UploadForm
 
 @app.errorhandler(404)
 def not_found_error(error):
@@ -17,3 +18,20 @@ def internal_error(error):
 def index():
     return render_template('index.html',
         title='Home')
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    form = UploadForm();
+    if form.validate_on_submit():
+        flash('Your changes have been saved.')
+        return redirect(url_for('done'))
+
+    return render_template('upload.html',
+        title='Upload a story',
+        form = form)
+
+
+@app.route('/done', methods=['GET'])
+def done():
+    return render_template('done.html',
+        title='Done!')
