812a9cbd9fe09427c09049bac746879b97df2e57
John Sarracino <jsarraci@cs.ucsd.edu>
2016-10-08 17:10:33 -0700

    fancy forms

--- mosaic/forms.py
+++ mosaic/forms.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, TextAreaField, FileField
+from wtforms import StringField, BooleanField, TextAreaField, FileField, SelectField
 from wtforms.validators import DataRequired, Length, regexp
 
 class UploadForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
-    email = StringField('email', validators=[DataRequired()])
-    country = StringField('country', validators=[DataRequired()])
-    city = StringField('city', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired()])
+    country = StringField('Country', validators=[DataRequired()])
+    city = StringField('City', validators=[DataRequired()])
     video = StringField('Video File Link', validators=[DataRequired()])
-    tags = StringField('tags', default="")
-    relationship = SelectField('relationship', choices=[('mom', 'erased mom')])
-    release = BooleanField('release', default=False, validators=[DataRequired()])
+    tags = StringField('Tags', default="")
+    relationship = SelectField('Relationship', choices=[('mom', 'erased mom')])
+    release = BooleanField('Release', default=False, validators=[DataRequired()])
