52f2f9c14f84880adc7047ab555d6a9e438a4635
Dimo <lami4ka@gmail.com>
2016-10-12 21:30:55 -0700

    Added submission_type (local upload vs. external url)

---
+++ db_repository/versions/005_migration.py
@@ -0,0 +1,34 @@
+from sqlalchemy import *
+from migrate import *
+
+
+from migrate.changeset import schema
+pre_meta = MetaData()
+post_meta = MetaData()
+submission = Table('submission', post_meta,
+    Column('id', Integer, primary_key=True, nullable=False),
+    Column('time', DateTime),
+    Column('submission_type', Enum('local', 'external')),
+    Column('url', String(length=1024)),
+    Column('author_id', Integer),
+    Column('country', String(length=120)),
+    Column('city', String(length=120)),
+    Column('relation', Enum('erased_mom')),
+    Column('ip', String(length=128)),
+    Column('other', String(length=1000)),
+)
+
+
+def upgrade(migrate_engine):
+    # Upgrade operations go here. Don't create your own engine; bind
+    # migrate_engine to your metadata
+    pre_meta.bind = migrate_engine
+    post_meta.bind = migrate_engine
+    post_meta.tables['submission'].columns['submission_type'].create()
+
+
+def downgrade(migrate_engine):
+    # Operations to reverse the above upgrade go here.
+    pre_meta.bind = migrate_engine
+    post_meta.bind = migrate_engine
+    post_meta.tables['submission'].columns['submission_type'].drop()
--- mosaic/models.py
+++ mosaic/models.py
@@ -1,6 +1,7 @@
 from mosaic import db
 
 relationships = [ "erased_mom" ]
+submission_types = [ "local", "external" ]
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -11,6 +12,7 @@ class User(db.Model):
 class Submission(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     time = db.Column(db.DateTime)
+    submission_type = db.Column(db.Enum(*submission_types), index=True, unique=True)
     url = db.Column(db.String(1024), index=True, unique=True)
     author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     country = db.Column(db.String(120), index=True, unique=True)
