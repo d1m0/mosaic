dd44c5917be8c5601c1ab792f0c0c49b0e34fdff
Dimo <lami4ka@gmail.com>
2016-10-12 21:32:41 -0700

    Removed unneccessary unique constraints from db models. (copy pasta cause?)

--- mosaic/models.py
+++ mosaic/models.py
@@ -5,18 +5,18 @@ submission_types = [ "local", "external" ]
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), index=True, unique=True)
+    name = db.Column(db.String(120), index=True)
     email = db.Column(db.String(120), index=True, unique=True)
     submissions = db.relationship('Submission', backref='author', lazy='dynamic')
 
 class Submission(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     time = db.Column(db.DateTime)
-    submission_type = db.Column(db.Enum(*submission_types), index=True, unique=True)
-    url = db.Column(db.String(1024), index=True, unique=True)
+    submission_type = db.Column(db.Enum(*submission_types), index=True)
+    url = db.Column(db.String(1024), index=True)
     author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    country = db.Column(db.String(120), index=True, unique=True)
-    city = db.Column(db.String(120), index=True, unique=True)
-    relation = db.Column(db.Enum(*relationships), index=True, unique=True)
-    ip = db.Column(db.String(128), index=True, unique=True)
-    other = db.Column(db.String(1000), index=True, unique=True)
+    country = db.Column(db.String(120), index=True)
+    city = db.Column(db.String(120), index=True)
+    relation = db.Column(db.Enum(*relationships), index=True)
+    ip = db.Column(db.String(128), index=True)
+    other = db.Column(db.String(1000), index=True)
